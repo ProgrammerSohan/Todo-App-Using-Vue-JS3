bd65acfc063e595667b843cc1cf666266cc0becc
ProgrammerSohan <sohanofficial2022@gmail.com>
2024-04-26 22:32:51 +0600

    2a_template & styling

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,43 @@
 <template>
-  <h1>hi</h1>
-
+   <Todo />
 </template>
  <script>
+import Todo from "./components/Todo.vue";
 
  export default {
      name: "App",
-    
-    
-    
+      components: { Todo },
+       
      };
  
  </script>
- <style scoped>
+ <style>
+ @import url('https://fonts.googleapis.com/css2?family=Jersey+25&display=swap');
+
+ *{
+   margin: 0;
+   padding: 0;
+   box-sizing: border-box;
+   list-style-type: none;
+ }
+
+body {
+  font-family: "Jersey 25", sans-serif;
+  font-weight: 400;
+  font-style: normal;
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
+  background-color: #2c3e50;
+  color:white;
+}
+
+
+ #app{
+  text-align: center;
+  margin-top: 60px;
+
+ }
 
  
  </style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="wrapper">
+       <h1> Todo By Sohan!!</h1>
+       <h2>Todo List</h2>
+    </div>
+</template>
+<script>
+export default{
+    name:"Todo",
+
+}
+
+</script>
+<style scoped>
+.wrapper {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+}
+
+</style>

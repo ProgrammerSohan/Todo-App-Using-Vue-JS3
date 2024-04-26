c0904bbebd46d973f82e2080519f8511fe594c58
ProgrammerSohan <sohanofficial2022@gmail.com>
2024-04-27 00:05:41 +0600

    2b_template style

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="wrapper">
        <h1> Todo By Sohan!!</h1>
-       <h2>Todo List</h2>
+       <h2 class="header">Todo List</h2>
+       <div class="inputDiv">
+            <input ref="myInput" type="text" class="todoInput" v-model="model" placeholder="Enter a new todo!" />
+       <button class="addTodoBtn">Add Todo </button>
+
+       </div>
     </div>
 </template>
 <script>
@@ -18,6 +23,37 @@ export default{
     flex-direction: column;
     justify-content: center;
     align-items: center;
+
+}
+.header {
+    color: orange;
+}
+.inputDiv{
+    width: 90%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 0.5rem;
+}
+.todoInput{
+    width: 70%;
+    padding: 5px;
+    border: none;
+    border-radius: 10px;
+
+}
+.addTodoBtn{
+    border: none;
+    padding: 5px;
+    border-radius: 5px;
+    background-color: green;
+    color: white;
+    cursor: pointer;
+
+}
+.addTodoBtn:hover{
+    background-color: navy;
+    color:rgb(11, 204, 140);
 
 }
 
